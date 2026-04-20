1d5770cbeba17ce5ea0dcafaaca5c0d40b5915ef
Derek Lin <david771005@gmail.com>
2026-04-20 22:47:47 +0800

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        // const arr = [...s.replace(/[^a-z0-9]/gi, '').toLowerCase()]
+
+        // let foward = 0;
+        // let backward = arr.length - 1;
+        // for(let i = 0; i < arr.length; i++) {
+        //     if(arr[foward] != arr[backward]) {
+        //         return false
+        //     } 
+        //     foward += 1;
+        //     backward -= 1;
+        // }
+        // return true;
+
+        const cleanStr = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+        const reversedStr = cleanStr.split('').reverse().join('');
+        
+        return cleanStr == reversedStr
+    }
+}
