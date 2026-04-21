7cba36d6fc44544629b9f9f633e80f3837364e73
Derek Lin <david771005@gmail.com>
2026-04-21 17:27:08 +0800

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        // Solution 1: [bruce force] using three pointers to iterate all possilbiity
+    
+        const resultMap = {};
+        for(let a = 0 ; a < nums.length; a++) {
+            for(let b = a + 1 ; b < nums.length; b++) {
+                for(let c = b + 1 ; c < nums.length ; c++) {
+                    if(nums[a] + nums[b] + nums[c] == 0) {
+                        let triplet = [nums[a], nums[b], nums[c]].sort();
+                        let key = triplet.join('');
+                        if(!resultMap[key]) resultMap[key] = triplet; 
+                    } 
+                }
+            }
+        }
+        
+        return Object.values(resultMap); // turn {"-101":[-1,0,1], "-202":[-2,0,2]} into {[-1,0,1],[-2,0,2]} 
+    }
+}
