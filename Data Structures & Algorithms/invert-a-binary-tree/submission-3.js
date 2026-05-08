d66873322d618429c124a1e0344b7634f410620c
Derek Lin <david771005@gmail.com>
2026-05-08 15:28:40 +0800

    Add: invert-a-binary-tree - submission-3

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-3.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+    invertTree(root) {
+
+        if(!root) return null;
+        if(!root.right && !root.left) return root;
+
+        [root.right, root.left] = [root.left, root.right];
+        this.invertTree(root.right);
+        this.invertTree(root.left);
+
+        return root;
+    }
+}
