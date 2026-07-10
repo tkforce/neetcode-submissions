a75d47cad71fc727775702e4a0dab7913affdf1e
Derek Lin <david771005@gmail.com>
2026-07-10 12:16:24 +0800

    Add: longest-increasing-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-2.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    lengthOfLIS(nums) {
+        // Solution 1: bructe-force
+        // 把每一個num當作出發點，分別算出其子序列，然後取最長的
+        // Solution 2: 增加一個memo
+
+        if(nums.length === 0) return 0;
+
+        const dp = Array(nums.length).fill(1);
+        dp[nums.length - 1] = 1;
+        let maxLength = 0;
+        for(let i = nums.length - 1 ; i >= 0 ; i--) {
+            for(let j = i + 1 ; j < nums.length ; j++) {
+                // 比較i以後的每一個j，如果比num[j] > num[i]都是合法的
+                // 所以就要取最大的dp[j]+1後當作dp[i]的值
+
+                if(nums[j] > nums[i]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        return maxLength;
+    }
+}
