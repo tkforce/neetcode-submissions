aa6701a2338644ff49573d982522dada7620e57d
Derek Lin <david771005@gmail.com>
2026-07-21 11:59:41 +0800

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        // [2,3,1,5], target = 4
+        // {
+        //     2: 0,
+        //     3: 1,
+        //     1: 2,
+        //     5: 3
+        // }
+        let map = new Map();
+        for(let i = 0; i < nums.length ; i++) {
+            map.set(nums[i], i);
+        }
+        for(let i = 0 ; i < nums.length ; i++){
+            let diff = target - nums[i];
+            if(map.has(diff) && map.get(diff) !== i) return [map.get(diff), i];
+        }
+        return [];
+    }
+}
