6b9f497ea58b6677f49bdb912e0d79af285b769f
Derek Lin <david771005@gmail.com>
2026-07-23 11:34:13 +0800

    Update: is-palindrome - submission-5

--- Data Structures & Algorithms/is-palindrome/submission-5.js
+++ Data Structures & Algorithms/is-palindrome/submission-5.js
@@ -4,27 +4,9 @@ class Solution {
      * @return {boolean}
      */
     isPalindrome(s) {
-        let l = 0, r = s.length - 1;
+        const cleanStr = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+        const reversed = cleanStr.split('').reverse().join('');
 
-        while (l < r) {
-            while (l < r && !this.isAlphanumeric(s[l])) l++;
-            while (r > l && !this.isAlphanumeric(s[r])) r--;
-
-            if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
-            l++;
-            r--;
-        }
-        return true;
-
-        // const cleanStr = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
-        // const reversedStr = cleanStr.split('').reverse().join('');
-        
-        // return cleanStr == reversedStr
-    }
-
-    isAlphanumeric(char) {
-        return (char >= 'a' && char <= 'z') || 
-               (char >= 'A' && char <= 'Z') || 
-               (char >= '0' && char <= '9');
+        return cleanStr === reversed;
     }
 }
