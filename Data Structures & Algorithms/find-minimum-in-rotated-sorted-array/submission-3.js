910b4abd92aede3c34d05888c261ba7d97e3c7b3
Derek Lin <david771005@gmail.com>
2026-07-28 14:42:27 +0800

    Add: find-minimum-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-3.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums) {
+        // find the pivot point using binary search
+        let l = 0;
+        let r = nums.length - 1;
+        
+        while(l < r) {
+            let mid = Math.floor((l + r) / 2) ;
+
+            if(nums[mid] > nums[r]) {
+                // pivot is on the right
+                l = mid + 1;
+            } else {
+                // pivot is on the left
+                r = mid;
+            }
+
+        }
+        return nums[r]
+    }
+}
