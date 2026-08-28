2f1c84a607df65d7edd7b55c15479bb4217c22b6
Derek Lin <david771005@gmail.com>
2026-08-28 10:50:17 +0800

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root) {
+        // use BFS and only collect the last one of that level
+
+        if(!root) return [];
+        const result = [];
+        const queue = [root];
+        let head = 0;
+        while(head < queue.length) {
+            let size = queue.length - head;
+            let level = [];
+            for(let i = 0 ; i < size ; i ++){
+                let node = queue[head];
+                level.push(node.val);
+                if(node.left) queue.push(node.left);
+                if(node.right) queue.push(node.right);
+                head++
+            }
+
+            result.push(level.pop());
+        }
+       return result;
+    }
+}
