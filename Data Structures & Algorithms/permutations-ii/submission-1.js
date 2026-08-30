43a3d24b4684f5805592591215fce8b31b33ebf8
Derek Lin <david771005@gmail.com>
2026-08-30 18:30:15 +0800

    Add: permutations-ii - submission-1

---
+++ Data Structures & Algorithms/permutations-ii/submission-1.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permuteUnique(nums) {
+        const result = [];
+        const path = [];
+        const used  = new Array(nums.length).fill(false);
+
+        const dfs = () => {
+            if(path.length === nums.length) {
+                result.push([...path]);
+                return;
+            }
+            // for each level, same num can only be picked once
+            // otherwise the result will have duplicated
+            const seen = new Set(); 
+            for(let i = 0 ; i < nums.length ; i ++) {
+                if(used[i] || seen.has(nums[i])) continue;
+                seen.add(nums[i]);
+                path.push(nums[i]);
+                used[i] = true;
+                dfs();
+                path.pop();
+                used[i] = false;
+            }
+        }
+        dfs();
+        return result;
+        
+    }
+}
